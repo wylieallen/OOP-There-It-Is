a4d0e48bbf8e3cd9d98f75234cd06e973cda463f
rmercker <merckerrobert@yahoo.com>
2018-04-14 09:57:59 -0400

    adding spawning interfaces

---
+++ src/Spawning/SpawnObservable.java
@@ -0,0 +1,12 @@
+package Spawning;
+
+/**
+ * Created by dontf on 4/14/2018.
+ */
+public interface SpawnObservable {
+
+    public void notifyAllOfSpawn (InfluenceArea IA);
+    public void registerObserver (SpawnObserver SO);
+    public void deregisterObserver (SpawnObserver SO);
+
+}
---
+++ src/Spawning/SpawnObserver.java
@@ -0,0 +1,10 @@
+package Spawning;
+
+/**
+ * Created by dontf on 4/14/2018.
+ */
+public interface SpawnObserver {
+
+    public void notifySpawn (InfluenceArea IA, GameObject spanwer);
+
+}
