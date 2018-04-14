989288e122c5576ca147f8d264d172b75a9e3b62
rmercker <merckerrobert@yahoo.com>
2018-04-14 11:05:40 -0400

    utility classes vector and coordinate

---
+++ src/Utilities/Coordinate.java
@@ -0,0 +1,10 @@
+package Utilities;
+
+/**
+ * Created by dontf on 4/14/2018.
+ */
+public class Coordinate {
+
+    // TODO: need to add coordinaty stuff here!
+
+}
---
+++ src/Utilities/Vector.java
@@ -0,0 +1,22 @@
+package Utilities;
+
+/**
+ * Created by dontf on 4/14/2018.
+ */
+public class Vector {
+
+    private Direction direction;
+    private double magnitude;
+
+    public void add (Vector other) {
+
+    }
+
+    public Direction getDirection () {
+        return direction;
+    }
+
+    public double getMagnitude () {
+        return magnitude;
+    }
+}
