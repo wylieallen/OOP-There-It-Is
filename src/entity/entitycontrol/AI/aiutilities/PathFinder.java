f5b62132a1def689e2c8521856420b2cda53ea94
rmercker <merckerrobert@yahoo.com>
2018-04-15 18:04:39 -0400

    saving current ai to push other branch

---
+++ src/entity/entitycontrol/AI/aiutilities/PathFinder.java
@@ -0,0 +1,26 @@
+package entity.entitycontrol.AI.aiutilities;
+
+import entity.entitymodel.Entity;
+import maps.tile.LocalWorldTile;
+import utilities.Coordinate;
+import utilities.Vector;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by dontf on 4/15/2018.
+ */
+public class PathFinder {
+
+    public static List<Vector> createLocalPath (Coordinate start, Coordinate end, Entity whom, Map <Coordinate, LocalWorldTile> map) {
+        Stack <Vector> path = new Stack<>();
+        Coordinate current = start;
+
+        
+
+        return path;
+    }
+
+}
