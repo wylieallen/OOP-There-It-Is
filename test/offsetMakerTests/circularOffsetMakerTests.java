0a645f8056676feccae108294a802cd3017cf7fd
jacobkissel <jacobkissel@ufl.edu>
2018-04-19 19:54:04 -0700

    Added circularOffsetPointsMaker and linear too, as well as tests for them. Currently circular is hella inefficent but ¯\_(ツ)_/¯

---
+++ test/offsetMakerTests/circularOffsetMakerTests.java
@@ -0,0 +1,115 @@
+package offsetMakerTests;
+
+import maps.Influence.circularOffsetPointsMaker;
+import maps.tile.Direction;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import utilities.Coordinate;
+
+import java.util.List;
+
+public class circularOffsetMakerTests {
+
+
+    @BeforeClass
+    public static void setup() {}
+
+    @Test
+    public void testCircleMaker() {
+        circularOffsetPointsMaker lineMaker = new circularOffsetPointsMaker();
+
+        Direction direction = Direction.N;
+
+        int radius = 0;
+        List<Coordinate> offsets = lineMaker.getOffsetPoints(direction, radius);
+        Assert.assertEquals(1,offsets.size());
+        Assert.assertTrue(containsRadiusZeroPoints(offsets));
+
+        radius = 1;
+        offsets = lineMaker.getOffsetPoints(direction, radius);
+        Assert.assertEquals(7,offsets.size());
+        Assert.assertTrue(containsRadiusZeroPoints(offsets));
+        Assert.assertTrue(containsRadiusOnePoints(offsets));
+
+
+        radius = 2;
+        offsets = lineMaker.getOffsetPoints(direction, radius);
+        Assert.assertEquals(19,offsets.size());
+        Assert.assertTrue(containsRadiusZeroPoints(offsets));
+        Assert.assertTrue(containsRadiusOnePoints(offsets));
+        Assert.assertTrue(containsRadiusTwoPoints(offsets));
+
+
+    }
+
+    private boolean containsRadiusZeroPoints(List<Coordinate> offsets) {
+        return offsets.contains(new Coordinate(0,0));
+    }
+
+    private boolean containsRadiusOnePoints(List<Coordinate> offsets){
+        boolean result = true;
+        if(!offsets.contains(new Coordinate(1,0))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(1,-1))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(0,-1))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-1,0))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-1,1))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(0,1))){
+            result = false;
+        }
+        return result;
+    }
+
+    private boolean containsRadiusTwoPoints(List<Coordinate> offsets){
+        boolean result = true;
+        if(!offsets.contains(new Coordinate(2,0))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(2,-1))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(2,-2))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(1,-2))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(0,-2))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-1,-1))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-2,0))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-2,1))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-2,2))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(-1,2))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(0,2))){
+            result = false;
+        }
+        if(!offsets.contains(new Coordinate(1,1))){
+            result = false;
+        }
+        return result;
+    }
+
+
+}
